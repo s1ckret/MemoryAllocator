d54fc0c646805a9dd9846dba0502792835445332
Kovalenko Ivan <s1ckretbox@gmail.com>
2020-01-24 12:28:35 +0200

    Add project configuration: Tests
    Add test folder and examples. Now, there is clear division between
    shared library, client part and tests.
    
    Signed-off-by: Kovalenko Ivan <s1ckretbox@gmail.com>

---
+++ src/AddFunction.cpp
@@ -0,0 +1,5 @@
+#include "AddFunction.h"
+
+int add( const int & lhs, const int & rhs ) {
+    return lhs + rhs;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,4 @@
-#include <string>
-
-#include "gtest/gtest.h"
-
-std::string getDeveloperName( ) {
-    return "S1ckret";
-}
-
-TEST( HelloWorld, DeveloperName ) {
-    EXPECT_EQ( "S1ckret", getDeveloperName( ) );
-}
 
 int main( int argc, char* argv[] ) {
-    ::testing::InitGoogleTest( &argc, argv );
-    return RUN_ALL_TESTS( );
+   return 0;
 }
