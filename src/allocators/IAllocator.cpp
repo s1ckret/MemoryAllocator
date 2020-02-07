2991b19ced3fa77097fc315ee0dc200a6fe80389
Kovalenko Ivan <s1ckretbox@gmail.com>
2020-02-07 20:06:43 +0200

    Feature: Add debug tools for IAllocator
    - _DEBUG macro
    - info() method that prints all information about allocator
    
    Signed-off-by: Kovalenko Ivan <s1ckretbox@gmail.com>

--- src/allocators/IAllocator.cpp
+++ src/allocators/IAllocator.cpp
@@ -2,10 +2,25 @@
 
 #include <cassert>
 
-IAllocator::IAllocator( ) {
+IAllocator::IAllocator( )
+    : _maxMemory( 0 )
+    , _usedMemory( 0 )
+    , _numAllocations( 0 ) 
+{
 
 }
 
 IAllocator::~IAllocator( ) {
     assert( _numAllocations == 0 && _usedMemory == 0 );
 }
+
+#ifdef _DEBUG
+#include <iostream>
+
+void IAllocator::info( ) {
+    std::cout << "Allocator status:\n";
+    std::cout << "Max Memory: " << _maxMemory << " bytes.\n";
+    std::cout << "Used Memory: " << _usedMemory << " bytes.\n";
+    std::cout << "Number of allocations: " << _numAllocations << ".\n";
+}
+#endif
--- src/allocators/IAllocator.h
+++ src/allocators/IAllocator.h
@@ -2,6 +2,10 @@
 
 #include <cstddef>
 
+#define _DEBUG
+
+using byte_t = char;
+
 class IAllocator {
 public:
     IAllocator( );
@@ -13,6 +17,10 @@ public:
     inline size_t getMaxMemory() const { return _maxMemory; };
     inline size_t getUsedMemory() const { return _usedMemory; };
     inline size_t getNumAllocations() const { return _numAllocations; };
+protected:
+#ifdef _DEBUG
+    virtual void info( );
+#endif
 protected:
     size_t _maxMemory;
     size_t _usedMemory;
